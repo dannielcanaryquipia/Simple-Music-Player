efff05446fc1169225ae22a2c7f4e58610b0c320
Danniel Canary Quipia <dannielcanaryq@gmail.com>
2024-12-01 16:27:58 +0800

    Music Player

---
+++ data.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+        name: 'Tears In Heaven',
+        path: 'musics/Eric Clapton - Tears In Heaven (Official Video).mp3',
+        artist: 'Eric Clapton',
+        cover: 'images/tearsinheaven.jpg',
+    },
+    {
+        name: 'Blue Jeans',
+        path: 'musics/GANGGA - Blue Jeans (Official Lyric Video).mp3',
+        artist: 'GANGGA',
+        cover: 'images/bluejeans.jpg',
+    },
+    {
+        name: 'How Lovely..',
+        path: 'musics/How Lovely is Your Dwelling Place - Himig Heswita (Lyric Video) (64 kbps).mp3',
+        artist: 'Himig Heswita How Lovely is Your Dwelling Place',
+        cover: 'images/howlovelyisyourdwellingplace.jpg',
+    },
+    {
+        name: 'Isang Taong Lumipas',
+        path: 'musics/Isang Taong Lumipas - Erwin Lacsa _ Ryan Cayabyab Singers.mp3',
+        artist: 'Erwin Lacsa_Ryan Cayabyab Singers',
+        cover: 'images/isangtaonglumipas.jpg',
+    },
+    {
+        name: 'Theres No Easy Way(Cover)',
+        path: 'musics/James Ingram - Theres No Easy Way _ H2O Band Official (Cover Video).mp3',
+        artist: 'James Ingram_H2O Band Official ',
+        cover: 'images/theresnoeasyway.jpg',
+    },
+    {
+        name: '_Say Yes_ by Floetry',
+        path: 'musics/KofiBMusic Performs _Say Yes_ by Floetry (As seen on TikTok).mp3',
+        artist: 'KofiBMusic',
+        cover: 'images/sayyesbyfloetry.jpg',
+    },
+    {
+        name: 'LANY Live_Acoustic',
+        path: 'musics/LANY Live_Acoustic Playlist (songs to study, work or chill)_oVHQaINM9E8.mp3',
+        artist: 'LANY (songs to study, work or chill)',
+        cover: 'images/lany.jpg',
+    },
+    {
+        name: 'Fallen Extended Version',
+        path: 'musics/Lola Amour - Fallen Extended Version (Karaoke Version).mp3',
+        artist: 'Lola Amour',
+        cover: 'images/lolaamour.jpg',
+    },
+    {
+        name: 'P.S. I LOVE YOU',
+        path: 'musics/Paul Partohap - P.S. I LOVE YOU (Lyric Video).mp3',
+        artist: 'Paul Partohap',
+        cover: 'images/psiloveyou.jpg',
+    },
+    {
+        name: 'Happiness (Official Audio)',
+        path: 'musics/Rex Orange County - Happiness (Official Audio).mp3',
+        artist: 'Rex Orange County',
+        cover: 'images/rexorangecountry.jpg',
+    }
+]
